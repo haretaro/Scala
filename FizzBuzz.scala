46947b7ac311191cb7ee507d21d942a5fe1b8ded
haretaro <hoge@example.com>
2015-06-19 21:06:12 +0900

    using placeholder

--- FizzBuzz.scala
+++ FizzBuzz.scala
@@ -1,13 +1,11 @@
 object FizzBuzz extends App{
 
 	print(1 to 100 map(
-		n => {
-			n match{
-				case n if n%15==0 => "FizzBuzz"
-				case n if n%3==0 => "Fizz"
-				case n if n%5==0 => "Buzz"
-				case _ => n.toString()
-			}
+		_ match{
+			case n if n%15==0 => "FizzBuzz"
+			case n if n%3==0 => "Fizz"
+			case n if n%5==0 => "Buzz"
+			case n => n.toString()
 		}
 	) mkString(" "))
 }
