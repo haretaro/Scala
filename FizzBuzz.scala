b30d8800f1156fddf7b8367a281eb700cd097c58
haretaro <hoge@example.com>
2015-06-19 20:36:14 +0900

    using function literal

--- FizzBuzz.scala
+++ FizzBuzz.scala
@@ -1,11 +1,13 @@
 object FizzBuzz extends App{
-	
-	def fizzBuzz( n:Int ): String = n match{
-		case n if n%15==0 => "FizzBuzz"
-		case n if n%3==0 => "Fizz"
-		case n if n%5==0 => "Buzz"
-		case _ => n.toString()
-	}
 
-	print((1 to 100).map(fizzBuzz).mkString(" "))
+	print(1 to 100 map(
+		n => {
+			n match{
+				case n if n%15==0 => "FizzBuzz"
+				case n if n%3==0 => "Fizz"
+				case n if n%5==0 => "Buzz"
+				case _ => n.toString()
+			}
+		}
+	) mkString(" "))
 }
