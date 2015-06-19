9370620af10eff5c4b4972ee71ec3ba590fbc6f0
haretaro <hoge@example.com>
2015-06-19 17:43:14 +0900

    using match

--- FizzBuzz.scala
+++ FizzBuzz.scala
@@ -1,15 +1,10 @@
 object FizzBuzz extends App{
 	
-	def fizzBuzz( n:Int ): String = {
-		if (n%3==0 && n%5==0){
-			return "FizzBuzz"
-		}else if(n%3==0){
-			return "Fizz"
-		}else if(n%5==0){
-			return "Buzz"
-		}else{
-			return n.toString()
-		}
+	def fizzBuzz( n:Int ): String = n match{
+		case n if n%15==0 => "FizzBuzz"
+		case n if n%3==0 => "Fizz"
+		case n if n%5==0 => "Buzz"
+		case _ => n.toString()
 	}
 
 	print((1 to 100).map(fizzBuzz).mkString(" "))
