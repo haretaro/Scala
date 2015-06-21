8f6c9ebf827b66ec3c1b77611e800e2b3dc7a1fa
haretaro <hoge@example.com>
2015-06-22 00:01:07 +0900

    fixed method call expression

--- FizzBuzz.scala
+++ FizzBuzz.scala
@@ -1,11 +1,11 @@
 object FizzBuzz extends App{
 
-	print(1 to 100 map(
+	println(1 to 100 map(
 		_ match{
 			case n if n%15==0 => "FizzBuzz"
 			case n if n%3==0 => "Fizz"
 			case n if n%5==0 => "Buzz"
-			case n => n.toString()
+			case n => n toString
 		}
 	) mkString(" "))
 }
