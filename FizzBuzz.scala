3ba22a67397647a6cd729766fe160177570eb7a8
Sen Tee <SEN@ELDRIDGE.local>
2015-06-23 19:28:20 +0900

    deleted 'match'

--- FizzBuzz.scala
+++ FizzBuzz.scala
@@ -1,11 +1,9 @@
 object FizzBuzz extends App{
 
 	println(1 to 100 map(
-		_ match{
-			case n if n%15==0 => "FizzBuzz"
-			case n if n%3==0 => "Fizz"
-			case n if n%5==0 => "Buzz"
-			case n => n toString
-		}
+		case n if n%15==0 => "FizzBuzz"
+		case n if n%3==0 => "Fizz"
+		case n if n%5==0 => "Buzz"
+		case n => n toString
 	) mkString(" "))
 }
