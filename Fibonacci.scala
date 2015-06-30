3e8a792e6d9bb08f81b473cccaceec66a5f0fe41
haretaro <frontend.1000@gmail.com>
2015-06-30 21:06:13 +0900

    now it works

--- Fibonacci.scala
+++ Fibonacci.scala
@@ -2,10 +2,12 @@ object Fibonacci extends App{
 
   def makeFibonacci() : (Int => BigInt)= {
     var memo = scala.collection.mutable.Map[Int,BigInt](1->BigInt(1),2->BigInt(1))
-    ( n => memo getOrElseUpdate(n,memo(n-1)+memo(n-2)))
+    def fibonacci(n:Int) : (BigInt) = memo getOrElseUpdate(n,fibonacci(n-1)+fibonacci(n-2))
+    fibonacci
   }
 
   val fibonacci = makeFibonacci
+  println( fibonacci(10))
   for (i <- 1 to 100)
     println( fibonacci(i) )
 }
