037c526a50df48f2d3617afde891144a17379f78
haretaro <frontend.1000@gmail.com>
2015-06-30 16:20:21 +0900

    ShowNumbers.scala

--- ShowNumbers.scala
+++ ShowNumbers.scala
@@ -3,11 +3,9 @@ import java.io.InputStreamReader
 
 object ShowNumbers extends App{
   val com = new BufferedReader(new InputStreamReader(System.in))
-  println(com.ready())
-
   var n = 1
   while(com.ready()==false){
     println(n)
-    n = n+df
+    n = n+1
   }
 }
