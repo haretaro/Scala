2d197e29ac8b8abf38113f95df2202ece5d4b3c4
haretaro <frontend.1000@gmail.com>
2015-08-26 14:54:36 +0900

    using wildcard

--- FizzBuzz.scala
+++ FizzBuzz.scala
@@ -1,11 +1,10 @@
 object FizzBuzz extends App{
 
   println(1 to 100 map(
-    n=>n match{
+    _ match{
       case n if n%15==0 => "FizzBuzz"
       case n if n%3==0 => "Fizz"
       case n if n%5==0 => "Buzz"
       case n => n.toString
-    }
-    ) mkString(" "))
+    }) mkString(" "))
 }
