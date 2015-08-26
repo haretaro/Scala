b54b008a93ae6c73fedc2d87f386b51298cc4779
haretaro <frontend.1000@gmail.com>
2015-08-26 14:42:39 +0900

    now it works

--- FizzBuzz.scala
+++ FizzBuzz.scala
@@ -1,9 +1,11 @@
 object FizzBuzz extends App{
 
-	println(1 to 100 map(
-		case n if n%15==0 => "FizzBuzz"
-		case n if n%3==0 => "Fizz"
-		case n if n%5==0 => "Buzz"
-		case n => n toString
-	) mkString(" "))
+  println(1 to 100 map(
+    n=>n match{
+      case n if n%15==0 => "FizzBuzz"
+      case n if n%3==0 => "Fizz"
+      case n if n%5==0 => "Buzz"
+      case n => n.toString
+    }
+    ) mkString(" "))
 }
